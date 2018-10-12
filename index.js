934b783c6468efc8ea8905b29e32211f8f1e3b28
cristian-g <cristian@cristiangonzalez.com>
2018-10-12 19:48:30 +0200

    App renamed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import AppViewContainer from './src/modules/AppViewContainer';
 import React, {Component} from 'react';
 import {AppRegistry} from 'react-native';
 
-class PepperoniAppTemplate extends Component {
+class App extends Component {
   render() {
     return (
       <Provider store={store}>
@@ -15,4 +15,4 @@ class PepperoniAppTemplate extends Component {
   }
 }
 
-AppRegistry.registerComponent('PepperoniAppTemplate', () => PepperoniAppTemplate);
+AppRegistry.registerComponent('App', () => App);
--- ios/AppTests/AppTests.m
+++ ios/AppTests/AppTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 180
 #define TEXT_TO_LOOK_FOR @"Increment counter"
 
-@interface PepperoniAppTemplateTests : XCTestCase
+@interface AppTests : XCTestCase
 
 @end
 
-@implementation PepperoniAppTemplateTests
+@implementation AppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
